cd797672c4e5d4ef11697c8986f2a2de93506ecc
emi kojima <emi8emi@gmail.com>
2018-02-13 01:25:16 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   ary = Array.new(4)
-  ary.unshift element
+  ary.index index_number
 end
 
 def retrieve_first_element_from_array(array)
